fa21016502cacd5d4ac8257dde20ec5679f4d5d8
Yi Zhong <zhongy@cbio.mskcc.org>
2014-02-17 12:57:19 -0800

    add TE.py

---
+++ src/TE.py
@@ -0,0 +1,10 @@
+import sys
+
+def usage():
+    sys.stderr.write('Usage:' + '\n' + 'python ReadInputs.py Experiment_Outline_File Gene_Count_File' + '\n')
+
+if __name__ == '__main__'
+    if len(sys.argv) < 3:
+        usage()
+    else:
+
