722553dd52e12b07bef87f40d647475493bc7684
Yi Zhong <zhongy@cbio.mskcc.org>
2014-11-20 11:46:15 -0500

    update

--- src/plot.py
+++ src/plot.py
@@ -8,6 +8,46 @@ import matplotlib.pyplot as plt
 from optparse import OptionParser, OptionGroup
 import pdb
 
+def parse_options(argv):
+
+    parser = OptionParser()
+
+    required = OptionGroup(parser, 'REQUIRED')
+    required.add_option('-k', action='store', type='string', dest='dataPkl', help='Binary file containing all source data and results of TE change analysis.')
+    required.add_option('-o', action='store', type='string', dest='outputPrefix', help='Specify the prefix of the output file name.')
+
+    optional = OptionGroup(parser, 'OPTIONAL')
+    optional.add_option('-p', action='store', type='string', dest='plotWhich', default='All', help='Which figure to be plotted. Options: EmpDisp, TE or All. [default: All]')
+
+    parser.add_option_group(required)
+    parser.add_option_group(optional)
+
+    (opts, args) = parser.parse_args(argv)
+
+    if len(argv) < 2:
+        parser.print_help()
+        sys.exit()
+
+    mandatories = ['dataPkl', 'outputPrefix']
+    for eachOpt in mandatories:
+        if not opts.__dict__[eachOpt]:
+            parser.error('-%s is a required option.\n' % eachOpt[0])
+        if eachOpt in mandatories[:1] and not os.path.exists(opts.__dict__[eachOpt]):
+            sys.stderr.write('\nError: File \'%s\' does not exist.\n\n' % opts.__dict__[eachOpt])
+            sys.exit()
+
+        if eachOpt == mandatories[1]:
+            if not os.path.dirname(opts.__dict__[eachOpt]):
+                opts.__dict__[eachOpt] = os.getcwd() + os.getcwd()[0] + opts.__dict__[eachOpt]
+            if not os.path.exists(os.path.dirname(opts.__dict__[eachOpt])):
+                sys.stderr.write('\nError: Path \'%s\' of the output file does not exist.\n\n' % os.path.dirname(opts.__dict__[eachOpt]))
+                sys.exit()
+
+    if opts.plotWhich not in ['EmpDisp', 'TE', 'All']:
+        parser.error('-p option can only take \'EmpDisp\', \'TE\' or \'All\' as argument.\n')
+
+    return opts
+
 def empDisp_scatter(data, fileOutName):
 
     cntRiboNorm = data.countRibo / data.libSizesRibo
@@ -215,77 +255,14 @@ def make_plots(data, opts):
         pos = opts.outFile.rfind('.')
         outputNamePrefix = opts.outFile[:pos]
 
-    if opts.dispDiff:
-        fileOutNameBoxplot = outputNamePrefix + '.disp.boxplot.pdf'
-        boxplot_disp(data.dispAdjRibo, data.dispAdjRna, fileOutNameBoxplot)
+    fileOutName = outputNamePrefix + '.EmpDisp.scatter.pdf'
+    empDisp_scatter(data, fileOutName)
+        
+    fileOutName = outputNamePrefix + '.EmpDisp.hist.pdf'
+    empDisp_hist(data, fileOutName)
 
-    if opts.dispDiff:
-        countRiboMean = np.mean(data.countRibo / data.libSizesRibo, axis=1)
-        countRiboMean = np.reshape(countRiboMean, (countRiboMean.size, 1))
-        fileOutNameDotplotRibo = outputNamePrefix + '.dispRibo.dotplot.pdf'
-        dotplot_disp(countRiboMean, data.dispRawRibo, data.dispAdjRibo, data.betaRibo, fileOutNameDotplotRibo, 'Ribo-Seq')
-
-        countRnaMean = np.mean(data.countRna / data.libSizesRna, axis=1)
-        countRnaMean = np.reshape(countRnaMean, (countRnaMean.size, 1))
-        fileOutNameDotplotRna = outputNamePrefix + '.dispRNA.dotplot.pdf'
-        dotplot_disp(countRnaMean, data.dispRawRna, data.dispAdjRna, data.betaRna, fileOutNameDotplotRna, 'RNA-Seq')
-    else:
-        countMean = np.mean(data.countRibo / data.libSizesRibo, axis=1)
-        countMean = np.reshape(countMean, (countMean.size, 1))
-        fileOutNameDotplot = outputNamePrefix + '.disp.dotplot.pdf'
-        dotplot_disp(countMean, data.dispRaw, data.dispAdj, data.beta, fileOutNameDotplot, '')
-
-    countMean = np.mean(data.countRibo / data.libSizesRibo, axis=1)
-    countMean = np.reshape(countMean, (countMean.size, 1))
-    logFoldChangeTE = data.logFoldChangeTE
-    if opts.dispDiff:
-        dispAdj = data.dispAdjRibo
-    else:
-        dispAdj = data.dispAdj
-    padj = data.padj
-    threshold = 0.1
-    fileOutNameDotplotTE = outputNamePrefix + '.TEchange.dotplot.pdf'
-    dotplot_TEchange(countMean, logFoldChangeTE, dispAdj, padj, threshold, fileOutNameDotplotTE)
-
-def parse_options(argv):
-
-    parser = OptionParser()
-
-    required = OptionGroup(parser, 'REQUIRED')
-    required.add_option('-k', action='store', type='string', dest='dataPkl', help='Binary file containing all source data and results of TE change analysis.')
-    required.add_option('-o', action='store', type='string', dest='outputPrefix', help='Specify the prefix of the output file name.')
-
-    optional = OptionGroup(parser, 'OPTIONAL')
-    optional.add_option('-p', action='store', type='string', dest='plotWhich', default='All', help='Which figure to be plotted. Options: EmpDisp, TE or All. [default: All]')
-
-    parser.add_option_group(required)
-    parser.add_option_group(optional)
-
-    (opts, args) = parser.parse_args(argv)
-
-    if len(argv) < 2:
-        parser.print_help()
-        sys.exit()
-
-    mandatories = ['dataPkl', 'outputPrefix']
-    for eachOpt in mandatories:
-        if not opts.__dict__[eachOpt]:
-            parser.error('-%s is a required option.\n' % eachOpt[0])
-        if eachOpt in mandatories[:1] and not os.path.exists(opts.__dict__[eachOpt]):
-            sys.stderr.write('\nError: File \'%s\' does not exist.\n\n' % opts.__dict__[eachOpt])
-            sys.exit()
-
-        if eachOpt == mandatories[1]:
-            if not os.path.dirname(opts.__dict__[eachOpt]):
-                opts.__dict__[eachOpt] = os.getcwd() + os.getcwd()[0] + opts.__dict__[eachOpt]
-            if not os.path.exists(os.path.dirname(opts.__dict__[eachOpt])):
-                sys.stderr.write('\nError: Path \'%s\' of the output file does not exist.\n\n' % os.path.dirname(opts.__dict__[eachOpt]))
-                sys.exit()
-
-    if opts.plotWhich not in ['EmpDisp', 'TE', 'All']:
-        parser.error('-p option can only take \'EmpDisp\', \'TE\' or \'All\' as argument.\n')
-
-    return opts
+    fileOutName = outputNamePrefix + 'Cnt-DeltaTE.scatter.pdf'
+    cnt_deltaTE_scatter(data, fileOutName)
 
 if __name__ == '__main__':
 
