c0625bbf0d94660354b9a8c4d695f1311387a261
Yi Zhong <zhongy@cbio.mskcc.org>
2014-11-20 22:48:17 -0500

    update

--- tools/nbinom_count_generator.py
+++ tools/nbinom_count_generator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
-""" This script generates two groups of negative binomial distributed discrete random variables. The two groups (condition A and condition B) contain equal number of  
-    entries (GENE0001, GENE0002, ... GENE1000, ...), and each entry can have multiple replicates for each condition. The mean of each entry is also negative binomial 
+""" This script generates two groups of negative binomial distributed discrete random variables. The two groups (condition 1 and condition 2) contain equal number of  
+    entries (GENE0001, GENE0002, ... GENE1000, ...), and each entry can have multiple replicates in each condition. The mean of each entry is also negative binomial 
     distributed. By using arguments '--beta1' and '--beta2', users can change the dispersion-mean relationship in order to generate different data set, for instance, 
     Ribo-Seq and RNA-Seq data. """
 
@@ -20,8 +20,8 @@ def parse_options(argv):
     required = OptionGroup(parser, 'REQUIRED')
 
     required.add_option('--numEntry', action='store', type='int', dest='numEntry', help='How many entries (genes / proteins) to be generated.')
-    required.add_option('--numSampleConA', action='store', type='int', dest='numSampleConA', help='How many samples / replicates to be generated for condition A.')
-    required.add_option('--numSampleConB', action='store', type='int', dest='numSampleConB', help='How many samples / replicates to be generated for condition B.')
+    required.add_option('--numSampleCon1', action='store', type='int', dest='numSampleConA', help='How many samples / replicates to be generated for condition A.')
+    required.add_option('--numSampleCon2', action='store', type='int', dest='numSampleConB', help='How many samples / replicates to be generated for condition B.')
     required.add_option('--output', action='store', type='string', dest='output', help='Output file name.')
 
     optional = OptionGroup(parser, 'OPTIONAL')
@@ -29,20 +29,20 @@ def parse_options(argv):
     optional.add_option('--nParamNB', action='store', type='float', dest='nParamNB', default=1, help='Assume the mean read count of each entry follows negative binomial distribution. ' \
                         'This argument is for the n parameter. [default: 1]')
     optional.add_option('--pParamNB', action='store', type='float', dest='pParamNB', default=0.01, help='Assume the mean read count of each entry follows negative binomial distribution. ' \
-                        'This argument is for the p parameter. It controls the read count level. For instance, use 0.001 for RNA-seq, and 0.001~0.01 for Ribo-seq. [default: 0.01]')
+                        'This argument is for the p parameter. It controls the read count level. For instance, use 0.001 for RNA-Seq, and 0.001~0.01 for Ribo-Seq. [default: 0.01]')
     optional.add_option('--beta1', action='store', type='float', dest='beta1', default=0.100, help='Assume disper = beta1 / MeanCount + beta2 (beta1 must > 0.0). [default: 0.1]')
     optional.add_option('--beta2', action='store', type='float', dest='beta2', default=0.001, help='Assume disper = beta1 / MeanCount + beta2 (beta2 must > 0.0). [default: 0.001]')
     optional.add_option('--dispFile', action='store', type='string', dest='dispFile', help='A text file contains dispersions from which the read count will be generated for every gene. ' \
-                        'The number of dispersions should equal to the number of entries. One dispersion per line in this text file. If this argument is on, the \'beta1\' and \'beta2\' ' \
+                        'The number of dispersions should equal to the number of entries. One dispersion per line in this text file. If this argument is given, the \'beta1\' and \'beta2\' ' \
                         'arguments will not be used.')
     optional.add_option('--addDisperError', action='store', type='float', dest='addDisperError', help='Add standard Gaussian distributed noise to log(dispersion) and use this dispersion ' \
                         'to generate read count. The value of this argument is the standard deviation of the Gaussian distribution. [defualt: no noise]')
-    optional.add_option('--numDiff', action='store', type='int', dest='numDiff', help='How many entries show different mean read count. This script generates half as increased mean count ' \
-                        'and half as decreased mean count. For example, 1001 out of 2000 genes showing different fold change, the program will generate 501 showing increase and 499 showing ' \
-                        'decrease. [default: no gene showing different]')
-    optional.add_option('--shapeGamma', action='store', type='float', dest='shapeGamma', default=2.0, help='Assume fold change of mean read count in two conditions follows gamma ' \
+    optional.add_option('--numDiff', action='store', type='int', dest='numDiff', help='How many entries show different mean read count. This script generates half entries as increased ' \
+                        'mean count and another half as decreased mean count. For example, 1001 out of 20000 genes showing different fold change, the program will generate 500 showing ' \
+                        'increase and 501 showing decrease. [default: no gene showing different]')
+    optional.add_option('--shapeGamma', action='store', type='float', dest='shapeGamma', default=1.5, help='Assume fold change of mean read count between two conditions follows gamma ' \
                         'distribution. Use this argument to set the shape paramter. [default: 1.5]')
-    optional.add_option('--scaleGamma', action='store', type='float', dest='scaleGamma', default=0.5, help='Assume fold change of mean read count in two conditions follows gamma ' \
+    optional.add_option('--scaleGamma', action='store', type='float', dest='scaleGamma', default=0.5, help='Assume fold change of mean read count between two conditions follows gamma ' \
                         'distribution. Use this argument to set the scale paramter. [default: 0.5]')
 
     parser.add_option_group(required)
@@ -109,23 +109,28 @@ def generate_count(options):
         numDiff = options.numDiff
 
         # The number of genes showing increased and decreased mean count is equal or 1 less. 
-        numDiffUp = round(numDiff / 2.0)
+        numDiffUp = numDiff / 2
         numDiffDn = numDiff - numDiffUp
 
         shapeParam = options.shapeGamma
         scaleParam = options.scaleGamma
 
-        # If fold change value of increased gene set is x, the decreased set is 1/x.
+        # Assume fold changes of mean count of different genes follow gamma distribution. If fold change value of increased gene set is x, the decreased set is 1/x.
         foldDiffUp = gamma.rvs(a=shapeParam, scale=scaleParam, loc=1.0, size=numDiffUp)
         foldDiffDn = 1.0 / gamma.rvs(a=shapeParam, scale=scaleParam, loc=1.0, size=numDiffDn)
-        foldDiff = np.hstack([foldDiffUp, foldDiffDn])
 
         # Fold change genes are randomly selected.
         idx = random.sample(range(numGene), numDiff)
+        idxUp = random.sample(idx, numDiffUp)
+        idxDn = np.setdiff1d(idx, idxUp)
 
-        # Change the mean count of condition B.
-        muB[idx] = muA[idx] * foldDiff
-        disper[idx] = disper[idx] / 10.0
+        # Change the mean count of condition B. Assume there is a negative correlation between mean count and fold change.
+        idxUpMem = np.argsort(mu[idxUp])
+        idxDnMem = np.argsort(mu[idxDn])
+        muNewUp = np.sort(mu[idxUp]) * np.sort(foldDiffUp)[::-1]
+        muNewDn = np.sort(mu[idxDn]) * np.sort(foldDiffDn)
+        muB[idxUp][idxUpMem] = muNewUp
+        muB[idxDn][idxDnMem] = muNewDn
 
     n = 1.0 / disper
     pA = n / (n + muA)
@@ -133,7 +138,7 @@ def generate_count(options):
 
     numDigits = len(str(numGene))
     with open(output, 'w') as FileOut:
-        FileOut.write('Entry\t' + 'ConditionA\t'*numSampleConA + 'ConditionB\t'*numSampleConB + 'Dispersion\t' + 'MeanCondA\t' + 'MeanCondB\t' + 'MeanFoldChange\n')
+        FileOut.write('Entry\t' + 'Condition1\t'*numSampleConA + 'Condition2\t'*numSampleConB + 'Dispersion\t' + 'MeanCond1\t' + 'MeanCond2\t' + 'MeanFoldChange\n')
         for i in range(numGene):
             z = numDigits - len(str(i+1))
             name = 'G' + '0'*z + str(i+1)
@@ -151,5 +156,5 @@ if __name__ == '__main__':
 
     options = parse_options(sys.argv)
     generate_count(options)
-    print 'Done!'
+    print '%s: Done!' % options.output
 
--- tools/nbinom_count_generator.sh
+++ tools/nbinom_count_generator.sh
@@ -2,16 +2,5 @@
 
 set -e
 
-#python nbinom_count_generator.py --numEntry 2000  --numSampleConA 3 --numSampleConB 3 --output ./test/disp012/Sim/RiboCnt1.0Fold.Beta1.0.txt --pParamNB 0.01 --beta1 1.0 --beta2 0.01
-#python nbinom_count_generator.py --numEntry 2000  --numSampleConA 3 --numSampleConB 3 --output ./test/disp012/Sim/RnaCnt1.0Fold.Beta0.1.txt --pParamNB 0.006 --beta1 0.1 --beta2 0.001
-#
-#python nbinom_count_generator.py --numEntry 2000  --numSampleConA 3 --numSampleConB 3 --output ./test/disp012/Sim/RiboCnt2.0Fold.Beta0.5.txt --pParamNB 0.01 --beta1 0.5 --beta2 0.001 --numDiff 1000 --meanFoldDiff 2.0
-#cat ./test/disp012/Sim/RiboCnt2.0Fold.Beta0.5.txt | tail +2 | cut -f 8 > ./test/disp012/Sim/dispersions.txt
-#python nbinom_count_generator.py --numEntry 2000  --numSampleConA 3 --numSampleConB 3 --output ./test/disp012/Sim/RnaCnt1.0Fold.useDisp.txt --pParamNB 0.006 --dispFile ./test/disp012/Sim/dispersions.txt
-
-
-#python nbinom_count_generator.py --numEntry 20000  --numSampleConA 10 --numSampleConB 10 --output ./test/disp012/Sim/RiboCnt2.0Fold.Beta0.1.Rep10.20K.txt --pParamNB 0.01 --beta1 0.1 --beta2 0.0001 --numDiff 1000 --meanFoldDiff 2.0
-#python nbinom_count_generator.py --numEntry 20000  --numSampleConA 10 --numSampleConB 10 --output ./test/disp012/Sim/RnaCnt1.0Fold.Beta0.1.Rep10.20K.txt --pParamNB 0.001 --beta1 0.1 --beta2 0.0001
-
-python nbinom_count_generator.py --numEntry 20000  --numSampleConA 3 --numSampleConB 3 --output ./test/disp012/Sim/RiboCnt.Beta0.1.Rep3.G20K.Diff1K.Sh2.0.Sc0.5.txt --pParamNB 0.01 --beta1 0.1 --beta2 0.0001 --numDiff 1000 --shapeGamma 2.0 --scaleGamma 0.5
-python nbinom_count_generator.py --numEntry 20000  --numSampleConA 3 --numSampleConB 3 --output ./test/disp012/Sim/RnaCnt.Beta0.1.Rep3.G20K.Diff0K.txt --pParamNB 0.001 --beta1 0.1 --beta2 0.0001
+python nbinom_count_generator.py --numEntry 20000  --numSampleConA 3 --numSampleConB 3 --output ../exp/Sim/Sim.Ribo.Rep3.G20K.Diff1K.Sh2.0.Sc0.5.cnt.txt --pParamNB 0.02 --beta1 0.1 --beta2 0.0001 --numDiff 1000 --shapeGamma 2.0 --scaleGamma 0.5
+python nbinom_count_generator.py --numEntry 20000  --numSampleConA 3 --numSampleConB 3 --output ../exp/Sim/Sim.Rna.Rep3.G20K.Diff0K.cnt.txt --pParamNB 0.001 --beta1 0.1 --beta2 0.0001
