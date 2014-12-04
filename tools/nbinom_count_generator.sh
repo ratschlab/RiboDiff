4530290dabd4636f9dc8793f85b8ed20c24c2f96
Yi Zhong <zhongy@cbio.mskcc.org>
2014-12-04 17:27:08 -0500

    update

--- tools/nbinom_count_generator.sh
+++ tools/nbinom_count_generator.sh
@@ -2,10 +2,11 @@
 
 set -e
 
+#Before Dec4
 #python nbinom_count_generator.py --numEntry 10000  --numSampleConA 3 --numSampleConB 3 --output ../exp/Sim/Sim.Rna.Rep3.G10K.Diff0K.cnt.txt --pParamNB 0.0008 --beta1 0.1 --beta2 0.0001
+#python nbinom_count_generator.py --numEntry 10000  --numSampleConA 3 --numSampleConB 3 --output ../exp/Sim/Sim.Ribo.Rep3.G10K3.Diff1K.Sh1.5.Sc0.5.cnt.txt --pParamNB 0.008 --beta1 0.1 --beta2 0.0001 --numDiff 1000 --shapeGamma 1.5 --scaleGamma 0.5
 
-#python nbinom_count_generator.py --numEntry 20000  --numSampleConA 3 --numSampleConB 3 --output ../exp/Sim/Sim.Rna.Rep3.G20K.Diff0K.cnt.txt --pParamNB 0.0008 --beta1 0.1 --beta2 0.0001
 
-python nbinom_count_generator.py --numEntry 10000  --numSampleConA 3 --numSampleConB 3 --output ../exp/Sim/Sim.Ribo.Rep3.G10K3.Diff1K.Sh1.5.Sc0.5.cnt.txt --pParamNB 0.008 --beta1 0.1 --beta2 0.0001 --numDiff 1000 --shapeGamma 1.5 --scaleGamma 0.5
-
+python nbinom_count_generator.py --numEntry 10000 --numSampleConA 3 --numSampleConB 3 --output ../exp/Sim/Sim.Ribo.Rep3.G10K.Diff5K.Sh1.5.Sc0.5.cnt.txt --nParamNB 0.5 --pParamNB 0.008 --beta1 0.1 --beta2 0.0001 --numDiff 5000 --shapeGamma 1.5 --scaleGamma 0.5
+python nbinom_count_generator.py --numEntry 10000 --numSampleConA 3 --numSampleConB 3 --output ../exp/Sim/Sim.Rna.Rep3.G10K.Diff0K.cnt.txt --nParamNB 1.0 --pParamNB 0.0002 --beta1 0.1 --beta2 0.0001
 
--- tools/run_TE.sh
+++ tools/run_TE.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-time python -B ../src/TE.py -e ../exp/Sim/ExpOutlineRep3.txt -c ../exp/Sim/Sim.Merged.Rep3.G10K3.Diff1KRb.Sh1.5.Sc0.5.cnt.txt -o ../exp/Sim/Sim.Merged.Rep3.G10K3.Diff1KRb.Sh1.5.Sc0.5.res0.txt -d 0 -r 1 -p 0
+#time python -B ../src/TE.py -e ../exp/Sim/ExpOutlineRep3.txt -c ../exp/Sim/Sim.Merged.Rep3.G10K3.Diff1KRb.Sh1.5.Sc0.5.cnt.txt -o ../exp/Sim/Sim.Merged.Rep3.G10K3.Diff1KRb.Sh1.5.Sc0.5.res0.txt -d 0 -r 1 -p 0
 
-sleep 300s
+#sleep 300s
 
-time python -B ../src/TE.py -e ../exp/Sim/ExpOutlineRep3.txt -c ../exp/Sim/Sim.Merged.Rep3.G10K3.Diff1KRb.Sh1.5.Sc0.5.cnt.txt -o ../exp/Sim/Sim.Merged.Rep3.G10K3.Diff1KRb.Sh1.5.Sc0.5.res1.txt -d 1 -r 1 -p 0
+time python -B ../src2/TE.py -e ../exp/Sim/ExpOutlineRep3.txt -c ../exp/Sim/Sim.Merged.Rep3.G10K3.Diff1KRb.Sh1.5.Sc0.5.cnt.txt -o ../exp/Sim/Sim.Merged.Rep3.G10K3.Diff1KRb.Sh1.5.Sc0.5.res4.txt -d 1 -r 1 -p 0
 
