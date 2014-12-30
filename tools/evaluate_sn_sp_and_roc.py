b696f6f0902c504fe147ef8614449068ee920ed1
Yi Zhong <zhongy@cbio.mskcc.org>
2014-12-29 20:01:01 -0500

    update

--- tools/evaluate_sn_sp_and_roc.py
+++ tools/evaluate_sn_sp_and_roc.py
@@ -6,7 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pdb
 
-def cal_sn_sp(simFileName, data):
+def cal_SN_SP(simFileName, data):
 
     diffMarker = np.loadtxt(simFileName, dtype=int, delimiter='\t', skiprows=1, usecols=(-1,))
     entryID = np.loadtxt(simFileName, dtype=str, delimiter='\t', skiprows=1, usecols=(0,))
@@ -16,38 +16,67 @@ def cal_sn_sp(simFileName, data):
     fdr = 0.15
     with np.errstate(invalid='ignore'):
         observedPosID = data.geneIDs[data.padj <= fdr]
-    pdb.set_trace()
-    sn = np.in1d(observedPosID, truePosID).nonzero()[0].size / float(truePosID.size)
-
-    for fdr in np.arange(0.05, 0.151, 0.025):
-        with np.errstate(invalid='ignore'):
-            detectPosID = data.geneIDs[data.padj <= fdr]
-        tpr = np.in1d(detectPosID, truePosID).nonzero()[0].size / float(truePosID.size)
-        fpr = np.in1d(detectPosID, truePosID, invert=True).nonzero()[0].size / float(trueNegID.size)
-        print 'FDR < %1.3f, True Positive Rate = %1.3f; False Positive Rate = %1.3f' % (fdr, tpr, fpr)
+        observedNegID = data.geneIDs[data.padj >  fdr]
+    sn = np.intersect1d(observedPosID, truePosID).size / float(truePosID.size)
+    sp = np.intersect1d(observedNegID, trueNegID).size / float(trueNegID.size)
+    print 'FDR < %1.2f, Sensitivity = %1.3f, Specificity = %1.3f' % (fdr, sn, sp)
 
     print '*'*20
 
-def cal_tpr_fpr_babel(simFileName, babelFileName):
+    idx = np.argsort(data.pval, axis=None)
+    geneIDs = data.geneIDs[idx]
+    SN = np.zeros(geneIDs.size)
+    SP = np.zeros(geneIDs.size)
+    P  = np.zeros(geneIDs.size)
+    for i in range(geneIDs.size):
+        sn = np.intersect1d(geneIDs[:i+1], truePosID).size / float(truePosID.size)
+        sp = np.intersect1d(geneIDs[i+1:], trueNegID).size / float(trueNegID.size)
+        SN[i] = sn
+        SP[i] = sp
+        P[i]  = data.pval[idx[i]][0]
 
-    diffMarker = np.loadtxt(simFileName, dtype=int, delimiter='\t', skiprows=1, usecols=(-1,))
-    entryID = np.loadtxt(simFileName, dtype=str, delimiter='\t', skiprows=1, usecols=(0,))
-    truePosID = entryID[diffMarker > 0]
-    trueNegID = entryID[diffMarker == 0]
+    return (SN, SP, P)
 
-    babelFDRstr = np.loadtxt(babelFileName, dtype=str, delimiter='\t', skiprows=1, usecols=(6,))
-    babelFDRstr[babelFDRstr=='NA'] = 'nan'
-    babelFDR = babelFDRstr.astype(float)
-    babelID = np.loadtxt(babelFileName, dtype=str, delimiter='\t', skiprows=1, usecols=(1,))
+def plot_sn_sp(SN1, SP1, P1, SN2, SP2, P2, SN3, SP3, P3, SN4, SP4, P4, SN5, SP5, P5, snspFigName):
 
-    for fdr in np.arange(0.05, 0.151, 0.025):
-        with np.errstate(invalid='ignore'):
-            detectPosID = babelID[babelFDR <= fdr]
-        tpr = np.in1d(detectPosID, truePosID).nonzero()[0].size / float(truePosID.size)
-        fpr = np.in1d(detectPosID, truePosID, invert=True).nonzero()[0].size / float(trueNegID.size)
-        print 'FDR < %1.3f, True Positive Rate = %1.3f; False Positive Rate = %1.3f' % (fdr, tpr, fpr)
+    fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(10, 5))
 
-    print '*'*20
+    ax1.plot(P1[~np.isnan(P1)], SN1[~np.isnan(P1)], linestyle='-', color='orange', label=r'Ribo, Gamma $\alpha$=1.5')
+    ax1.plot(P2[~np.isnan(P2)], SN2[~np.isnan(P2)], linestyle='-', color='skyblue', label=r'RNA, Gamma $\alpha$=1.5')
+    ax1.plot(P3[~np.isnan(P3)], SN3[~np.isnan(P3)], linestyle='--', color='orange', label=r'Ribo, Gamma $\alpha$=0.8')
+    ax1.plot(P4[~np.isnan(P4)], SN4[~np.isnan(P4)], linestyle='--', color='skyblue', label=r'RNA, Gamma $\alpha$=0.6')
+    ax1.plot(P5[~np.isnan(P5)], SN5[~np.isnan(P5)], linestyle='-', color='tomato', label=r'Ribo & RNA, combined')
+
+    ax1.legend(loc='lower right', handlelength=3, prop={'size':7})
+
+    ax1.get_xaxis().tick_bottom()
+    ax1.get_yaxis().tick_left()
+    ax1.tick_params(axis='x', labelsize=8)
+    ax1.tick_params(axis='y', labelsize=8)
+
+    ax1.set_title(r'Sensitivity', fontsize=10)
+    ax1.set_xlabel(r'$P\/value$', fontsize=10)
+    ax1.set_ylabel(r'$Sensitivity$', fontsize=10)
+
+    ax2.plot(P1[~np.isnan(P1)], SP1[~np.isnan(P1)], linestyle='-', color='orange', label=r'Ribo, Gamma $\alpha$=1.5')
+    ax2.plot(P2[~np.isnan(P2)], SP2[~np.isnan(P2)], linestyle='-', color='skyblue', label=r'RNA, Gamma $\alpha$=1.5')
+    ax2.plot(P3[~np.isnan(P3)], SP3[~np.isnan(P3)], linestyle='--', color='orange', label=r'Ribo, Gamma $\alpha$=0.8')
+    ax2.plot(P4[~np.isnan(P4)], SP4[~np.isnan(P4)], linestyle='--', color='skyblue', label=r'RNA, Gamma $\alpha$=0.6')
+    ax2.plot(P5[~np.isnan(P5)], SP5[~np.isnan(P5)], linestyle='-', color='tomato', label=r'Ribo & RNA, combined')
+
+    #ax2.legend(loc='lower left', handlelength=3, prop={'size':7})
+
+    ax2.get_xaxis().tick_bottom()
+    ax2.get_yaxis().tick_left()
+    ax2.tick_params(axis='x', labelsize=8)
+    ax2.tick_params(axis='y', labelsize=8)
+
+    ax2.set_title(r'Specificity', fontsize=10)
+    ax2.set_xlabel(r'$P\/value$', fontsize=10)
+    #ax2.set_xlabel(r'$\log_{10}(P\/value)$', fontsize=7)
+    ax2.set_ylabel(r'$Specificity$', fontsize=10)
+
+    plt.savefig(snspFigName, format='pdf', bbox_inches='tight') 
 
 def cal_TPR_FPR(simFileName, data):
 
@@ -56,17 +85,24 @@ def cal_TPR_FPR(simFileName, data):
     truePosID = entryID[diffMarker > 0]
     trueNegID = entryID[diffMarker == 0]
 
+    fdr = 0.15
+    with np.errstate(invalid='ignore'):
+        observedPosID = data.geneIDs[data.padj <= fdr]
+    tpr = np.intersect1d(observedPosID, truePosID).size / float(truePosID.size)
+    fpr = np.setdiff1d(observedPosID, truePosID).size / float(trueNegID.size)
+    print 'FDR < %1.2f, True positive rate = %1.3f, False positive rate = %1.3f' % (fdr, tpr, fpr)
+
+    print '*'*20
+
     idx = np.argsort(data.pval, axis=None)
     geneIDs = data.geneIDs[idx]
-    TPR = []
-    FPR = []
+    TPR = np.zeros(geneIDs.size)
+    FPR = np.zeros(geneIDs.size)
     for i in range(geneIDs.size):
-        tpr = np.in1d(geneIDs[0:i+1], truePosID).nonzero()[0].size / float(truePosID.size)
-        fpr = np.in1d(geneIDs[0:i+1], truePosID, invert=True).nonzero()[0].size / float(trueNegID.size)
-        TPR.extend([tpr])
-        FPR.extend([fpr])
-
-    print 'calculate TPR, FPR: Done!'
+        tpr = np.intersect1d(geneIDs[:i+1], truePosID).size / float(truePosID.size)
+        fpr = np.setdiff1d(geneIDs[:i+1], truePosID).size / float(trueNegID.size)
+        TPR[i] = tpr
+        FPR[i] = fpr
 
     return (TPR, FPR)
 
@@ -80,29 +116,39 @@ def cal_TPR_FPR_babel(simFileName, babelFileName):
     babelPvalstr = np.loadtxt(babelFileName, dtype=str, delimiter='\t', skiprows=1, usecols=(5,))
     babelPvalstr[babelPvalstr=='NA'] = 'nan'
     babelPval = babelPvalstr.astype(float)
+    babelPadjstr = np.loadtxt(babelFileName, dtype=str, delimiter='\t', skiprows=1, usecols=(6,))
+    babelPadjstr[babelPadjstr=='NA'] = 'nan'
+    babelPadj = babelPadjstr.astype(float)
     babelID = np.loadtxt(babelFileName, dtype=str, delimiter='\t', skiprows=1, usecols=(1,))
+
+    fdr = 0.15
+    with np.errstate(invalid='ignore'):
+        observedPosID = babelID[babelPadj <= fdr]
+    tpr = np.intersect1d(observedPosID, truePosID).size / float(truePosID.size)
+    fpr = np.setdiff1d(observedPosID, truePosID).size / float(trueNegID.size)
+    print 'FDR < %1.2f, True positive rate = %1.3f, False positive rate = %1.3f' % (fdr, tpr, fpr)
+
+    print '*'*20
 
     idx = np.argsort(babelPval, axis=None)
     geneIDs = babelID[idx]
-    TPR = []
-    FPR = []
+    TPR = np.zeros(geneIDs.size)
+    FPR = np.zeros(geneIDs.size)
     for i in range(geneIDs.size):
-        tpr = np.in1d(geneIDs[0:i+1], truePosID).nonzero()[0].size / float(truePosID.size)
-        fpr = np.in1d(geneIDs[0:i+1], truePosID, invert=True).nonzero()[0].size / float(trueNegID.size)
-        TPR.extend([tpr])
-        FPR.extend([fpr])
-
-    print 'calculate TPR, FPR: Done!'
+        tpr = np.intersect1d(geneIDs[:i+1], truePosID).size / float(truePosID.size)
+        fpr = np.setdiff1d(geneIDs[:i+1], truePosID).size / float(trueNegID.size)
+        TPR[i] = tpr
+        FPR[i] = fpr
 
     return (TPR, FPR)
 
-def plot_roc(TPR1, FPR1, TPR2, FPR2, TPR3, FPR3, rocFileName):
+def plot_roc(TPR1, FPR1, TPR2, FPR2, TPR3, FPR3, rocFigName):
 
     fig, ax = plt.subplots()
 
     ax.plot(FPR1, TPR1, linestyle='-', color='tomato', label='Single dispersion')
-    ax.plot(FPR2, TPR2, linestyle='-', color='blue', label='Two dispersions')
-    ax.plot(FPR3, TPR3, linestyle='-', color='violet', label='babel')
+    ax.plot(FPR2, TPR2, linestyle='-', color='skyblue', label='Two dispersions')
+    ax.plot(FPR3, TPR3, linestyle='-', color='orange', label='babel')
 
     ax.legend(loc='lower right', prop={'size':11})
 
@@ -115,28 +161,59 @@ def plot_roc(TPR1, FPR1, TPR2, FPR2, TPR3, FPR3, rocFileName):
     ax.set_xlabel(r'$False\/positive\/rate$', fontsize=15)
     ax.set_ylabel(r'$True\/positive\/rate$', fontsize=15)
 
-    plt.savefig(rocFileName, format='pdf')
-
-    print 'plot: Done!'
+    plt.savefig(rocFigName, format='pdf', bbox_inches='tight')
 
 if __name__ == '__main__':
 
-    simFileName1 = sys.argv[1]
+    simFileName1 = '../exp/Sim/Sim.Ribo.Rep3.G2K.Diff1K.Sh1.5.Sc0.5.cnt.txt'
+    simFileName2 = '../exp/Sim/Sim.Rna.Rep3.G2K.Diff1K.Sh1.5.Sc0.5.cnt.txt'
+    simFileName3 = '../exp/Sim/Sim.Ribo.Rep3.G2K.Diff1K.Sh0.8.Sc0.5.cnt.txt'
+    simFileName4 = '../exp/Sim/Sim.Rna.Rep3.G2K.Diff1K.Sh0.6.Sc0.5.cnt.txt'
+    simFileName5 = '../exp/Sim/Sim.Ribo.Rep3.G2K.Diff1K.Sh0.8.Sc0.5.cnt.txt'
 
-    with open(sys.argv[2], 'rb') as FileIn:
+    resTEtest1 = '../exp/Sim/Sim.Merged.Rep3.G2K.Diff1KRb.Sh1.5.Sc0.5.res0.pkl'
+    resTEtest2 = '../exp/Sim/Sim.Merged.Rep3.G2K.Diff1KRna.Sh1.5.Sc0.5.res0.pkl'
+    resTEtest3 = '../exp/Sim/Sim.Merged.Rep3.G2K.Diff1KRb.Sh0.8.Sc0.5.res0.pkl'
+    resTEtest4 = '../exp/Sim/Sim.Merged.Rep3.G2K.Diff1KRna.Sh0.6.Sc0.5.res0.pkl'
+    resTEtest5 = '../exp/Sim/Sim.Merged.Rep3.G2K.Diff1KRbRna.Sh0.8Rb.Sh0.6Rna.Sc0.5.res0.pkl'
+
+    snspFigName = '../exp/Sim/Sim.Merged.Rep3.G2K.Diff1K.SnSp.res0.pdf'
+
+    with open(resTEtest1, 'rb') as FileIn:
         data1 = pickle.load(FileIn)
+    with open(resTEtest2, 'rb') as FileIn:
+        data2 = pickle.load(FileIn)
+    with open(resTEtest3, 'rb') as FileIn:
+        data3 = pickle.load(FileIn)
+    with open(resTEtest4, 'rb') as FileIn:
+        data4 = pickle.load(FileIn)
+    with open(resTEtest5, 'rb') as FileIn:
+        data5 = pickle.load(FileIn)
 
-    #with open(sys.argv[3], 'rb') as FileIn:
-    #    data2 = pickle.load(FileIn)
+    print 'Start SN1, SP1'
+    SN1, SP1, P1 = cal_SN_SP(simFileName1, data1)
+    print 'Start SN2, SP2'
+    SN2, SP2, P2 = cal_SN_SP(simFileName2, data2)
+    print 'Start SN3, SP3'
+    SN3, SP3, P3 = cal_SN_SP(simFileName3, data3)
+    print 'Start SN4, SP4'
+    SN4, SP4, P4 = cal_SN_SP(simFileName4, data4)
+    print 'Start SN5, SP5'
+    SN5, SP5, P5 = cal_SN_SP(simFileName5, data5)
+    plot_sn_sp(SN1, SP1, P1, SN2, SP2, P2, SN3, SP3, P3, SN4, SP4, P4, SN5, SP5, P5, snspFigName)
+    print 'plot SN SP: Done!\n'
 
-    #babelFileName = sys.argv[4]
+    babelFileName = '../exp/Sim/Sim.RnaRibo.Rep3.G2K.Diff1K.Sh0.8Rb.Sh0.6Rna.Sc0.5.res.babel.txt'
+    rocFigName = '../exp/Sim/Sim.Rep3.G2K.Diff1KRbRna.Sh0.8Rb.Sh0.6Rna.Sc0.5.roc.pdf'
+    resTEtest6 = '../exp/Sim/Sim.Merged.Rep3.G2K.Diff1KRbRna.Sh0.8Rb.Sh0.6Rna.Sc0.5.res1.pkl'
+    with open(resTEtest6, 'rb') as FileIn:
+        data6 = pickle.load(FileIn)
 
-    #rocFileName = sys.argv[-1]
-
-    cal_sn_sp(simFileName1, data1)
-    #cal_tpr_fpr(simFileName, data2)
-    #cal_tpr_fpr_babel(simFileName, babelFileName)
-    #TPR1, FPR1 = cal_TPR_FPR(simFileName, data1)
-    #TPR2, FPR2 = cal_TPR_FPR(simFileName, data2)
-    #TPR3, FPR3 = cal_TPR_FPR_babel(simFileName, babelFileName)
-    #plot_roc(TPR1, FPR1, TPR2, FPR2, TPR3, FPR3, rocFileName)
+    print 'Start TPR1, FPR1'
+    TPR1, FPR1 = cal_TPR_FPR(simFileName5, data5)
+    print 'Start TPR2, FPR2'
+    TPR2, FPR2 = cal_TPR_FPR(simFileName5, data6)
+    print 'Start TPR3, FPR3'
+    TPR3, FPR3 = cal_TPR_FPR_babel(simFileName5, babelFileName)
+    plot_roc(TPR1, FPR1, TPR2, FPR2, TPR3, FPR3, rocFigName)
+    print 'plot ROC: Done!'
