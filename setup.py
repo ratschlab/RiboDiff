d50edc88f30456e0aeee8c60b56e7998a86dcb26
Yi Zhong <zhongy@cbio.mskcc.org>
2015-03-10 11:35:38 -0400

    update codes

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+"""
+Installation scripts based on these:
+http://www.scotttorborg.com/python-packaging/minimal.html
+http://packages.python.org/distribute/setuptools.html
+"""
+
+from setuptools import setup
+
+def readme():
+    with open('README') as f:
+       return f.read()
+
+
+setup(name='RiboDiff',
+      version='0.1',
+      description='Ribosome foot printing...',
+      long_description=readme(),
+      keywords='ribo-seq rna-seq differential-expression-testing translational-efficiency',
+      url='http://galaxy.cbio.mskcc.org',
+      author=['Yi Zhong', 'Vipin T. Sreedharan'],
+      author_email=['zhongy@cbio.mskcc.org', 'vipin@cbio.mskcc.org'], 
+      license='GPLv3',
+      packages=['ribodiff'],
+      package_dir={'ribodiff': 'src/ribodiff'},
+      install_requires=['numpy', 'scipy', 'statsmodels', 'matplotlib'],
+      test_suite="test",
+      zip_safe=False)
