ee5fbed0827adbca56370abd2f20a2506a59121f
Vipin <vipin@cbio.mskcc.org>
2015-03-16 17:09:14 -0400

    checking for the installation test

--- test/test_TE.py
+++ test/test_TE.py
@@ -10,11 +10,13 @@ setup_file = sys.modules['__main__'].__file__
 print setup_file
 setup_dir = os.path.abspath(os.path.dirname(setup_file))
 print setup_dir
+sub_module_dir = os.path.join(setup_dir, "src")
+print sub_module_dir
 
-test_cmd = "python src/ribodiff/TE.py -e test/exp_outline.txt -c test/test_gene_count_table.txt -o test/test_result.txt -d 0 -r 1 -p 1 -q 0.1"
+test_cmd = "python %s/scripts/TE.py -e %s/test/exp_outline.txt -c %s/test/test_gene_count_table.txt -o %s/test/test_result.txt -d 0 -r 1 -p 1 -q 0.1" % (setup_dir, setup_dir, setup_dir, setup_dir)
 
 try:
-    os.chdir(setup_dir)
+    os.chdir(sub_module_dir)
     test_process = subprocess.Popen(test_cmd, shell=True)
     test_process.wait()
 except Exception, e:
