d658076e773375edaa3209a5c53d9361a9c2e256
Vipin <vipin@cbio.mskcc.org>
2015-03-18 14:49:26 -0400

    validating the return code from test run to make sure that testrun was successfull

--- test/TE_test.py
+++ test/TE_test.py
@@ -18,9 +18,12 @@ test_cmd = "python %s/scripts/TE.py -e %s/test/exp_outline.txt -c %s/test/test_g
 try:
     os.chdir(sub_module_dir)
     test_process = subprocess.Popen(test_cmd, shell=True)
-    test_process.wait()
+    returncode = test_process.wait()
+
+    if returncode != 0:
+        raise Exception, "Exit status return code = %i" % returncode
+
+    print "Test run finished. Please check the results in the %s/test/ directory." % setup_dir
+
 except Exception, e:
     print "Error running test cases.\n%s" % str(e) 
-    sys.exit(0)
-
-print "Test run finished. Please check the results in the ./test/ directory."
