49e2ad724f1c233873edba49c3340134bd05c5dd
Yi Zhong <zhongy@cbio.mskcc.org>
2015-04-01 11:58:17 -0400

    change input file name

--- tests/TE_test.py
+++ tests/TE_test.py
@@ -20,7 +20,7 @@ class TransEfficiency(unittest.TestCase):
         setup_file = sys.modules['__main__'].__file__
         setup_dir = os.path.abspath(os.path.dirname(setup_file))
 
-        test_cmd = "python src/ribodiff/functional_test_te.py -e tests/exp_outline.txt -c tests/test_gene_count_table.txt -o tests/test_result.txt -d 0 -r 1 -p 1 -q 0.1"
+        test_cmd = "python src/ribodiff/functional_test_te.py -e tests/experimental_design.csv -c tests/test_gene_count_table.txt -o tests/test_result.txt -d 0 -r 1 -p 1 -q 0.1"
 
         try:
             test_process = subprocess.Popen(test_cmd, shell=True)
