20aca6757bfe498679c582dc62ae11fdb701351f
Yi Zhong <zhongy@cbio.mskcc.org>
2015-04-24 10:22:07 -0400

    fix bug, output header line.

--- src/ribodiff/writeres.py
+++ src/ribodiff/writeres.py
@@ -29,11 +29,11 @@ def write_result(data, opts):
         dispAdjRibo = data.dispAdjRibo.astype(str)
         dispAdjRna  = data.dispAdjRna.astype(str)
         outNdarrayUnsorted = np.hstack([geneIDs, dispAdjRibo, dispAdjRna, pval, padj, TEctl, TEtrt, logFoldChangeTE])
-        header = 'geneIDs\tdisperRibo\tdisperRNA\tdisperConv\tpval\tpadj\tTE%s\tTE%s\tlog2FC_TE(%s vs %s)\t' % (nameCondA, nameCondB, nameCondB, nameCondA)
+        header = 'geneIDs\tdisperRibo\tdisperRNA\tpval\tpadj\tTE%s\tTE%s\tlog2FC_TE(%s vs %s)\t' % (nameCondA, nameCondB, nameCondB, nameCondA)
     else:
         dispAdj = data.dispAdj.astype(str)
         outNdarrayUnsorted = np.hstack([geneIDs, dispAdj, pval, padj, TEctl, TEtrt, logFoldChangeTE])
-        header = 'geneIDs\tdisper\tdisperConv\tpval\tpadj\tTE%s\tTE%s\tlog2FC_TE(%s vs %s)\t' % (nameCondA, nameCondB, nameCondB, nameCondA)
+        header = 'geneIDs\tdisper\tpval\tpadj\tTE%s\tTE%s\tlog2FC_TE(%s vs %s)\t' % (nameCondA, nameCondB, nameCondB, nameCondA)
 
     if opts.rankResult == 0:
         outNdarray = outNdarrayUnsorted.copy()
